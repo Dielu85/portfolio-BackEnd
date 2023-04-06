e162b9706ef1776164ba1b4477db5bedda0b1571
Dielu85 <114363152+Dielu85@users.noreply.github.com>
2023-04-06 19:18:54 -0300

    issue

---
+++ PruebaJSP/src/java/persistencia/PersonaJpaController.java
@@ -0,0 +1,140 @@
+package persistencia;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import logica.Persona;
+import persistencia.exceptions.NonexistentEntityException;
+
+
+public class PersonaJpaController implements Serializable {
+
+    public PersonaJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+    public PersonaJpaController() {
+        emf = Persistence.createEntityManagerFactory("PruebaJSPPU");
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    /**
+     *
+     * @param persona
+     */
+    public void create(Persona persona) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(persona);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Persona persona) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            persona = em.merge(persona);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                int id = persona.getId();
+                if (findpersona(id) == null) {
+                    throw new NonexistentEntityException("The persona with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(int id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Persona persona;
+            try {
+                persona = em.getReference(Persona.class, id);
+                persona.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The persona with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(persona);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Persona> findpersonaEntities() {
+        return findpersonaEntities(true, -1, -1);
+    }
+
+    public List<Persona> findpersonaEntities(int maxResults, int firstResult) {
+        return findpersonaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Persona> findpersonaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Persona.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Persona findpersona(int id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Persona.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getpersonaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Persona> rt = cq.from(Persona.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ PruebaJSP/src/java/servlets/SvPersona.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import logica.Controladora;
+import logica.Persona;
+
+@WebServlet(name = "SvPersona", urlPatterns = {"/SvPersona"})
+public class SvPersona extends HttpServlet {
+    
+    Controladora control = new Controladora();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {      
+    }
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+            
+        String dni = request.getParameter("dni");
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String telefono = request.getParameter("telefono");
+        
+        Persona pers = new Persona (0, dni, nombre, apellido, telefono);
+        control.crearPersona(pers);
+        
+    }    
+        
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
